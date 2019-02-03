2add27d6809678ff3b4bb979242dd4e3a56b3125
Po-Yu Chuang <ratbert.chuang@gmail.com>
2019-02-03 12:00:14 -0800

    picknames.py: add reload button

--- picknames.py
+++ picknames.py
@@ -15,16 +15,7 @@ class NameSelectController(object):
 
 
     def __init__(self, parent_view):
-        self.candidate_words = set()
-        self.word1_selected_count = {}
-        self.word2_selected_count = {}
-        self.word1_refused_count = {}
-        self.word2_refused_count = {}
-        self.refused_names = set()  # (w1, w2)
-        self.selected_names = set() # (w1, w2)
-
-        self.candidate_names = []   # (w1, w2, score)
-        self.candidate_name = None  # (w,1 w2)
+        self.reset_state()
 
         self.frame = tkinter.Frame(parent_view)
         self.frame.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
@@ -51,6 +42,25 @@ class NameSelectController(object):
 
         # restore state
         self.load_state()
+
+
+    def reset_state(self):
+        self.candidate_words = set()
+        self.word1_selected_count = {}
+        self.word2_selected_count = {}
+        self.word1_refused_count = {}
+        self.word2_refused_count = {}
+        self.refused_names = set()  # (w1, w2)
+        self.selected_names = set() # (w1, w2)
+
+        self.candidate_names_with_score = []   # (w1, w2, score)
+        self.candidate_name = None  # (w,1 w2)
+
+
+    def reload_state(self):
+        self.reset_state()
+        self.load_state()
+
 
     def load_state(self):
         selected_spelling_sound_words_mapping = {}
@@ -90,6 +100,9 @@ class NameSelectController(object):
         #names = [w1 + w2 for (w1, w2) in self.refused_names]
         #self.refused_slb.setlist(names)
 
+        self.update_candidate_names_with_score()
+
+
     def save_state(self):
         with open(self.SELECTED_NAMES_FILE_NAME, 'w', encoding='utf-8') as f:
             names = sorted([w1 + w2 for (w1, w2) in self.selected_names])
@@ -101,11 +114,13 @@ class NameSelectController(object):
             for name in names:
                 f.write(name + '\n')
 
+
     def add_selected_name(self, w1, w2):
         self.selected_count_sanity_check(w1, w2)
         self.word1_selected_count[w1] += 1
         self.word2_selected_count[w2] += 1
         self.selected_names.add((w1, w2))
+
 
     def add_refused_name(self, w1, w2):
         self.refused_count_sanity_check(w1, w2)
@@ -113,17 +128,20 @@ class NameSelectController(object):
         self.word2_refused_count[w2] += 1
         self.refused_names.add((w1, w2))
 
+
     def selected_count_sanity_check(self, w1, w2):
         if w1 not in self.word1_selected_count:
             self.word1_selected_count[w1] = 0
         if w2 not in self.word2_selected_count:
             self.word2_selected_count[w2] = 0
 
+
     def refused_count_sanity_check(self, w1, w2):
         if w1 not in self.word1_refused_count:
             self.word1_refused_count[w1] = 0
         if w2 not in self.word2_refused_count:
             self.word2_refused_count[w2] = 0
+
 
     def score_name(self, w1, w2):
         self.selected_count_sanity_check(w1, w2)
@@ -141,7 +159,8 @@ class NameSelectController(object):
             score -= float(self.word2_refused_count[w2]) / len(self.refused_names)
         return score
 
-    def update_candidate_names(self):
+
+    def update_candidate_names_with_score(self):
         names = []
         for w1 in self.candidate_words:
             for w2 in self.candidate_words:
@@ -149,14 +168,15 @@ class NameSelectController(object):
                     continue
                 score = self.score_name(w1, w2)
                 names.append((w1, w2, score))
-        self.candidate_names = sorted(names, key=lambda tup: tup[2])
-        #print(self.candidate_names)
-        self.num_candidates_label.config(text=len(self.candidate_names))
-        self.update_name_for_selection()
+        self.candidate_names_with_score = sorted(names, key=lambda tup: tup[2])
+        #print(self.candidate_names_with_score)
+        self.num_candidates_label.config(text=len(self.candidate_names_with_score))
+        self.update_current_candidate_name()
 
-    def update_name_for_selection(self):
-        if self.candidate_names:
-            (w1, w2, score) = self.candidate_names.pop()
+
+    def update_current_candidate_name(self):
+        if self.candidate_names_with_score:
+            (w1, w2, score) = self.candidate_names_with_score.pop()
             self.candidate_name = (w1, w2)
             name = w1 + w2
             self.candidate_label.config(text=name)
@@ -168,24 +188,27 @@ class NameSelectController(object):
             self.select_button.config(state=tkinter.DISABLED)
             self.refuse_button.config(state=tkinter.DISABLED)
 
+
     def select_current_candidate_name(self):
         (w1, w2) = self.candidate_name
         self.add_selected_name(w1, w2)
         self.update_selected_names_view()
-        #self.update_name_for_selection()
-        self.update_candidate_names()
+        #self.update_current_candidate_name()
+        self.update_candidate_names_with_score()
+
 
     def update_selected_names_view(self):
         names = sorted([w1 + w2 for (w1, w2) in self.selected_names])
         self.selected_slb.setlist(names)
+
 
     def refuse_current_candidate_name(self):
         (w1, w2) = self.candidate_name
         self.add_refused_name(w1, w2)
         #names = [w1 + w2 for (w1, w2) in self.refused_names]
         #self.refused_slb.setlist(names)
-        #self.update_name_for_selection()
-        self.update_candidate_names()
+        #self.update_current_candidate_name()
+        self.update_candidate_names_with_score()
 
 
 class App(object):
@@ -197,23 +220,31 @@ class App(object):
         self.button = tkinter.Button(root, text='離開', fg="red", command=self.quit)
         self.button.pack(side=tkinter.RIGHT)
 
+        self.button = tkinter.Button(root, text='重新載入', fg="red", command=self.reload)
+        self.button.pack(side=tkinter.RIGHT)
+
         self.button = tkinter.Button(root, text='儲存後離開', fg="red", command=self.save_and_quit)
         self.button.pack(side=tkinter.RIGHT)
 
         self.button = tkinter.Button(root, text='儲存', fg="red", command=self.save)
         self.button.pack(side=tkinter.RIGHT)
 
-        self.nsc.update_candidate_names()
 
     def save(self):
         self.nsc.save_state()
+
 
     def save_and_quit(self):
         self.nsc.save_state()
         self.root.quit()
 
+
     def quit(self):
         self.root.quit()
+
+
+    def reload(self):
+        self.nsc.reload_state()
 
 
 def main():
